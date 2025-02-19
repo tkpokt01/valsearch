ae26b6b651d806b43481ade13240350066aaa99a
TK <tkpokt@hotmail.com>
2025-02-19 14:23:18 +0900

    analytics

--- api/validators.mjs
+++ api/validators.mjs
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { Analytics } from "@vercel/analytics/react"
 
 // Get current directory path
 const __filename = fileURLToPath(import.meta.url);
